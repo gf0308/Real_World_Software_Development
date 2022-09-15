06f37ffbc10a58fb1fd87397c7ab5225b6e47019
gf0308 <kjbk0308@gmail.com>
2022-09-15 17:23:24 +0900

    2022.09.15 가변인자(varargs) 연습 커밋

---
+++ src/main/java/chapter2/BankTransactionAnalyzerSimple.java
@@ -0,0 +1,27 @@
+package main.java.chapter2;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class BankTransactionAnalyzerSimple {
+    private static final String RESOURCES = "src/main/resources/"; // 리소스의 기본 경로를 지정(상수)
+
+    public static void main(final String... args) throws IOException {
+
+//        final Path path = Paths.get(RESOURCES + "BankTransaction.csv");
+        final Path path = Paths.get(RESOURCES + args[0]);
+        final List<String> lines = Files.readAllLines(path);
+        double total = 0d;
+        for(final String line : lines) {
+            final String[] columns = line.split(",");
+            final double amount = Double.parseDouble(columns[1]);
+            total += amount;
+        }
+
+        System.out.println("The total for all transactions is " + total);
+    }
+
+}
